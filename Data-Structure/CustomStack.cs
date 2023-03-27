3b8fbd8b4f5c3b48edb34996212e58441a18b410
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-27 10:46:24 +0530

    [Rutuja]:Adding Custom Stack using generics

---
+++ Data-Structure/CustomStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structure
+{
+    internal class CustomStack<T>
+    {
+        public Node<T> top;
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (top == null)
+            {
+                top = newNode;
+                Console.WriteLine("{0} is inserted into Stack", top.data);
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+                Console.WriteLine("{0} is inserted into Stack", top.data);
+            }
+        }
+        
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+
+        public void Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Console.WriteLine("{0} is Removed from the Stack", top.data);
+                top = top.next;
+            }
+        }
+        public void isEmpty()
+        {
+            while (top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+        public void Display()
+        {
+            if (top == null)
+                Console.WriteLine("Stack is Empty");
+            else
+            {
+                Console.WriteLine("STACK");
+                Node<T> temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
+
--- Data-Structure/Program.cs
+++ Data-Structure/Program.cs
@@ -7,23 +7,44 @@ namespace Data_Structure
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Data structure");
-            CustomLinkedList<int> list = new CustomLinkedList<int>();
-            list.AddLast(10);
-            list.AddLast(20);
-            list.AddLast(30);
-            list.Display();
-            Console.WriteLine("-------------");
-            list.AddFirst(1);
-            list.AddFirst(2);
-            list.AddFirst(3);
-            list.RemoveFirst();
-            list.RemoveLast();
+            Console.WriteLine("Select given options");
+            Console.WriteLine("1.Custom Linked List \n2,Custom Stack ");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CustomLinkedList<int> list = new CustomLinkedList<int>();
+                    list.AddLast(56);
+                    list.AddLast(30);
+                    list.AddLast(70);
+                    list.Display();
+                    Console.WriteLine("-------------");
+                    list.AddFirst(1);
+                    list.AddFirst(2);
+                    list.AddFirst(3);
+                    list.RemoveFirst();
+                    list.RemoveLast();
 
-            list.Search(2);
-            list.InsertAfterParticularNode(40, 10);
-            list.DeleteInBetweenelement(40);
-            list.GetSize();
-            list.Display();
+                    list.Search(30);
+                    list.InsertAfterParticularNode(40, 30);
+                    list.DeleteInBetweenelement(40);
+                    list.GetSize();
+                    list.Display();
+                    break;
+                    case 2:
+                    CustomStack<int> stack = new CustomStack<int>();
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.Display();
+                    stack.Pop();
+                    stack.Display();
+                    stack.Peek();
+                    stack.Display();
+                    stack.isEmpty();
+                    stack.Display();
+                    break;
+            }
         }
     }
 }
